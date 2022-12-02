06206ec7d077913ee44ebe84e333688a6f6d6d07
Kunal Charde <kunalcharde1@gmail.com>
2022-12-02 20:15:44 +0530

    solution

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const input_area = document.getElementById("evaluatedText");
+const wordCount=document.getElementById("wordCount");
+
+
+
+input_area.addEventListener("input",()=>{ 
+    let txt =input_area.value.trim();
+    wordCount.textContent=txt.split(/\s+/).filter(
+        (item) => item).length;
+    });
